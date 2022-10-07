a2b0b4386d5271acf341c2ae642ffae729861fbd
EmilianoBonvicino <bonvicinoemilianoepet20@gmail.com>
2022-10-06 23:59:08 -0300

    first commit

---
+++ JS/script.js
@@ -0,0 +1,48 @@
+//*region Var
+var nombre1 = "Hola";
+let nombre2 = 'Que tal?';
+const nombre3 = `Bien... me alegro`;
+var arrayfin = [10] ;
+var num1 = 4 ;
+var num2 = 5 ;
+//* fin region var
+
+
+
+//*region calculos
+function calculoSuma(num1,num2){
+    var totalS = num1+num2;
+    return totalS;
+}
+
+function calculoResta(num1,num2){
+    var totalR = num1-num2;
+    return totalR;
+}
+
+function calculoMultiplicacion (num1, num2){
+    var totalM = num1*num2;
+    return totalM;
+}
+
+function calculoDivision (num1, num2){
+    var totalD = num1/num2;
+    return totalD;
+}
+//* fin region calculos
+
+//*region for
+    for (value of fin){
+        inicio + 1 ;
+    }
+//*fin region for
+
+//*region mostrar
+console.log(nombre1) ;
+
+console.log(nombre2) ;
+
+console.log(nombre3) ;
+
+confirm.log (inicio) ;
+//*fin region mostrar
